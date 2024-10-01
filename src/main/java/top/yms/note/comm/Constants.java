2ad9b3461b681d382b3b69b85f44a0f6e88ed727
yangmingsen <yangmingsen16@outlook.com>
2024-10-02 05:30:56 +0800

    修改prov地址配置

--- src/main/java/top/yms/note/comm/Constants.java
+++ src/main/java/top/yms/note/comm/Constants.java
@@ -8,8 +8,8 @@ public abstract class Constants {
     public final static String userid = "userid";
     public final static String MONGO = "mongo";
     public final static String MYSQL = "mysql";
-    public final static String BASE_URL = "http://api.notetest.yms.top/note/file/view?id=";
-    public final static String BASE_TMP_VIEW_URL = "http://api.notetest.yms.top/note/file/tmpView?id=";
+    public final static String BASE_URL = "http://api.note.yms.top/note/file/view?id=";
+    public final static String BASE_TMP_VIEW_URL = "http://api.note.yms.top/note/file/tmpView?id=";
     public final static String MONGO_FILE_SITE = "note.file";
 
     public final static String markdownSuffix = "md";
